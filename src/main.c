38c0410b1fa5245d9c547adda2357837805017a1
Ahmad Ayaz Amin <ayaza2004@outlook.com>
2024-03-10 10:40:18 -0400

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -1,7 +1,18 @@
+#include <stdio.h>
 #include <raylib.h>
+#include "aalloc.h"
 
 int main(int argc, char **argv)
 {
+  Arena arena = create_arena(sizeof(int));
+  int *a = aalloc(&arena, sizeof(int));
+  *a = 4;
+  printf("A is %d", *a);
+  aclear(&arena);
+  int *b = aalloc(&arena, sizeof(int));
+  *b = 5;
+  printf("B is %d", *b);
+  
   InitWindow(800, 600, "Test");
   SetTargetFPS(120);
 
@@ -12,6 +23,9 @@ int main(int argc, char **argv)
     DrawText("Success", 400, 300, 40, BLACK);
     EndDrawing();
   }
+
+  aclear(&arena);
+  destroy_arena(arena);
     
   return 0;
 }
